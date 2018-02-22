23d51d167d0433f3ebfbc055e57cadf23cefd451
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-02-22 14:51:43 -0800

    simple math operations

--- sec-2/meeting_idle.py
+++ sec-2/meeting_idle.py
@@ -1,3 +1,18 @@
 # Variables
 first_number = 1 + 1
 print(first_number)
+
+
+# simpleMath
+
+five = 2 + 3
+six  = 9 - 3
+ten  = 5 * 2
+four = 8 / 2
+one  = 9 % 4
+
+print('five: ', five)
+print('six: ',six)
+print('ten: ', ten)
+print('four: ', four)
+print('one: ', one)

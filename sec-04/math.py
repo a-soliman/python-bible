1322f0b9a164546bc80cdd48c939bbde61a7d9ce
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-02-23 07:04:55 -0800

    sec 4 done

---
+++ sec-04/math.py
@@ -0,0 +1,14 @@
+# MATH
+import math
+
+# ROUNDING
+print(round(2.1))
+print(round(1.5))
+
+# FLOORING ROUND
+print(math.floor(1.9))
+print(math.floor(2.8))
+
+# CELILLING
+print(math.ceil(2.1))
+print(math.ceil(3.2))

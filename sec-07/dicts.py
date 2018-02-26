e8e7f23225273a63db20a7280f4ea83d3c08084a
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-02-26 08:51:06 -0800

    getting keys and values && converting them into iterble lists using the list() methos

--- sec-07/dicts.py
+++ sec-07/dicts.py
@@ -15,3 +15,14 @@ print(students)
 #deleting
 del students["Bob"]
 print(students)
+
+
+# keys
+keys = list(students.keys())
+
+print(keys[1])
+
+
+# values
+values = list(students.values())
+print(values)

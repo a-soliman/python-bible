242b3c5e9091d71d0336390812aab097687506d2
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-02-26 08:10:54 -0800

    adding to a list through append

--- sec-07/lists.py
+++ sec-07/lists.py
@@ -1,8 +1,18 @@
 our_list = [4, 14, 55, 60, 17]
 
-
-print(our_list)
+# Type
 print(type(our_list))
 
+# table list
 our_table = [ [1,2,3] , [4,5,6] , [7,8,9] ]
 print(our_table[0][1])
+
+
+# ADDING TO LIST
+a = [2,4,6,7]
+print(a)
+
+
+# adding through append
+a.append('Ahmed')
+

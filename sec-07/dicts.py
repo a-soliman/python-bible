566f749eddb75e366f588a157d37b71f3879334b
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-02-26 09:03:36 -0800

    dict of lists and dicts of dicts

--- sec-07/dicts.py
+++ sec-07/dicts.py
@@ -26,3 +26,31 @@ print(keys[1])
 # values
 values = list(students.values())
 print(values)
+
+students2 = {
+	"Alice": 
+		{
+			"ID": "ID0001",
+			"age": 26,
+			"grade": "A"
+		}, 
+	"Bob": {
+			"ID": "ID0001",
+			"age": 32,
+			"grade": "B"
+		},
+	"Claire": {
+			"ID": "ID0001",
+			"age": 17,
+			"grade": "C"
+		},
+	"Dan": {
+			"ID": "ID0001",
+			"age": 24,
+			"grade": "D"
+		}
+}
+
+print(students2["Dan"]["age"])
+clire_info = list([students2["Claire"]["ID"]])
+print(clire_info)

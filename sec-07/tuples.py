4e18ea1f1b2208ab0496c9042cc503ab5daaeab0
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-02-26 08:39:25 -0800

    assinging vars using tuples and lists

--- sec-07/tuples.py
+++ sec-07/tuples.py
@@ -1,3 +1,20 @@
 our_tuble = (1,2,3,'a','b','c')
 
 print(our_tuble[0:3])
+
+a = [1,2,3]
+
+b = tuple(a)
+print(b)
+
+
+
+#assining variables using tuples and lists
+
+c,d,e = (3,4,5)
+
+print(e)
+
+f,g,h = [100, 200, 300]
+
+print(g)

b01d64035dbdae928d6ef2c5059509203d103ea9
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-02-26 15:43:49 -0800

    looping though

---
+++ sec-08/for.py
@@ -0,0 +1,24 @@
+vowels = 0
+cons = 0
+
+for letter in "Ahmed":
+	if letter.lower() in 'aeiou':
+		vowels = vowels +1
+
+	elif letter == ' ':
+		pass
+
+	else:
+		cons = cons + 1
+
+#print('{} Vows., {} cons.'.format(vowels, cons))
+
+students = {
+	"male": ["Tom", "John", "Mike", "Dan"],
+	"female": ["Sarah", "Huda", "Emily"]
+}
+
+for key in students.keys():
+	for name in students[key]:
+		if 'a' in name:
+			print(name)
--- sec-08/while.py
+++ sec-08/while.py
@@ -4,3 +4,4 @@ while i <= 100:
 	if i % 2 == 0:
 		print(i)
 	i = i + 1
+

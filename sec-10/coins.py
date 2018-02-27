2214306d03d41cb5d9434fac0b1e3ef9a4bb38e7
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-02-27 11:33:16 -0800

    added the needed methods for the coin class

--- sec-10/coins.py
+++ sec-10/coins.py
@@ -1,11 +1,59 @@
+import random
+
+
+
 class Pound:
-	value = 1.00
-	color = "gold"
-	num_edges = 1
-	diameter = 22.5
-	thickness = 3.15
-	heads = True
+
+	def __init__( self, rare=False ):
+
+		if rare == True:
+			self.value = 1.25
+		else: 
+			self.value = 1.00
+
+		self.color = "gold"
+		self.num_edges = 1
+		self.diameter = 22.5
+		self.thickness = 3.15
+		self.heads = True
+
+	def __del__( self):
+		print('Coin Spent!')
+		return 
+
+	def rust( self ):
+		self.color = "greenish"
+		return
+
+	def clean( self ):
+		self.color = "gold"
+		return
+
+	def turn_around( self ):
+		if self.heads == True:
+			self.heads = False
+		else: 
+			self.heads = True
+		return
+
+	def flip( self ):
+		options = [ True, False ]
+		choice = random.choice(options)
+		self.heads = choice
+		return
+
 
 coin1 = Pound()
+coin1.rust()
+print(coin1.color)
+coin1.clean()
+print(coin1.color)
+print(coin1.heads)
+coin1.flip()
+print(coin1.heads)
+coin1.flip()
+print(coin1.heads)
+coin1.flip()
+print(coin1.heads)
 
-print(type(coin1))
+del coin1

8f58aa49c85992a0fd250e4c71914bd181eb9e43
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-02-27 08:57:58 -0800

    packing and unpacking keyword arguments

--- sec-09/pack-unpack.py
+++ sec-09/pack-unpack.py
@@ -20,3 +20,18 @@ def add( *numbers ):
 	return total
 
 print(add(1,2,3,4,5,6,7,8,9))
+
+print('='*20)
+
+# UNPACKING KEYWORD ARGS
+def about( name, age, likes):
+	sentence = 'Meet {}, he is {} Y.O, and he likes {}.'.format(name, age, likes)
+	return sentence
+
+print(about('John', 21, 'Python'))
+
+dictionary = {"name": "Ahmed", "age": 30, "likes": "JS and Python"}
+
+print('=' * 10)
+
+print(about(**dictionary))

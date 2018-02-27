ddec99fe9430c983a590f3e74633ade587f8c1a3
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-02-27 08:25:46 -0800

    expolring global values modifing in lists and dicts

--- sec-09/scope.py
+++ sec-09/scope.py
@@ -12,3 +12,19 @@ def f2():
 f1()
 f2()
 print(a)
+
+myList = [ 2,4,5 ]
+
+print(myList)
+
+def f3():
+	myList[0] = 11
+	print(myList)
+
+def f4():
+	myList = 44
+	print(myList)
+
+f3()
+f4()
+print(myList)
